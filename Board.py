a7a08e1af269f0c07fdd8caba8e400981e8ae0d8
Robert Pal <92271019+rj-pal@users.noreply.github.com>
2025-02-10 13:29:55 -0500

    Update Board Class with win checking

--- Board.py
+++ Board.py
@@ -1,63 +1,183 @@
+from typing import Union, Optional
+from collections import Counter
+
+def int_converter(number, columns):
+    return divmod(number, columns)
+
+def winner_info(a, b, c, d):
+    print(f"Winner {a} is type {b} in Row {c + 1} and Column {d + 1}")
+
+   
 class Board:
     def __init__(self, rows: int, columns: int):
         self.rows = rows
         self.columns = columns
-        self.board = self._initialize_board()
+        self.board: list[list[Union[int, str]]] = self._initialize_board()
     
-    def _initialize_board(self) -> list[list]:
-        board = []
-        for i in range(self.rows):
-            if i == 1 or i == 5:
-                board.append([1 for _ in range(self.columns)])
-            else:
-                board.append([0 for _ in range(self.columns)])
-        return board
+    def _initialize_board(self) -> list[list[Union[int, str]]]:
+        """Initialize board in empty state."""
+        return [[0] * self.columns for _ in range(self.rows)]
     
-    def get_rows(self) -> list[list]:
+    def get_rows(self) -> list[list[int]]:
+        """Make a list of all the rows in the board."""
         return self.board
     
-    def get_columns(self) -> list[list]:
-        return list(map(list, zip(*self.board)))
+    def get_columns(self) -> list[list[int]]:
+        """Make a list of all columns in the board."""
+        return [list(col) for col in zip(*self.board)]
     
-    def get_left_diagonal(self) -> list:
-        return [self.board[i][-(i + 1)] for i in range(len(self.board))]
-    
-    def get_right_diagonals(self, dimension: int) -> list:
-        column_space = self.rows - dimension + 1
-        row_space = self.columns - dimension + 1
-        print({column_space, row_space})
-        right_diagonals = []
-        for i in range(column_space):
-            
-            for j in range(row_space):
-                print(f"ROW {i}, COL{j}")
-                right_diagonals.append([self.board[i + n][j+ n] for n in range(4)])
-                # print(right_diagonals)
-        return right_diagonals
-
-    def get_right_diagonal(self) -> list:
-        return [self.board[i][i] for i in range(len(self.board))]
+    def get_diagonals(self, dimension: int, direction: str) -> list[list[int]]:
+        """Make a list of all diagonals of given dimension in the 'right' or 'left' direction in the board."""
+        if dimension > min(self.rows, self.columns):
+            return []
+        diagonals = []
+        for i in range(self.rows - dimension + 1):
+            for j in range(self.columns - dimension + 1):
+                if direction == "right":
+                    diagonals.append([self.board[i + n][j + n] for n in range(dimension)])
+                elif direction == "left":
+                    diagonals.append([self.board[i + n][(self.columns - 1) - (j + n)] for n in range(dimension)])
+        return diagonals
     
     def square_is_occupied(self, row: int, column: int) -> bool:
+        """Checks if a square is empty."""
         return self.board[row][column] != 0
     
-    def update_square(self, row: int, column: int, square: str) -> None:
-        self.board[row][column] = square
-    
-    def update_board(self, row: int, column: int, marker: str) -> None:
-        self.update_square(row, column, marker)
-    
+    def add_to_square(self, row: int, column: int, value: Union[int, str]) -> bool:
+        """Adds a value to an empty square. Returns True if successful, False otherwise."""
+        if 0 <= row < self.rows and 0 <= column < self.columns:
+            if not self.square_is_occupied(row, column):  # Only add if empty
+                self.board[row][column] = value
+                return True
+        return False  # Move invalid or square occupied
+
+    def update_square(self, row: int, column: int, value: Union[int, str]) -> bool:
+        """Updates a square regardless of occupancy. Returns True if successful, False otherwise."""
+        if 0 <= row < self.rows and 0 <= column < self.columns:
+            self.board[row][column] = value  # Allow modification
+            return True
+        return False  # Invalid index
+
     def reset_board(self) -> None:
-        self.board = [[0] * self.columns for _ in range(self.rows)]
+        """Returns board to empty state."""
+        self.board = self._initialize_board()
+
+    def _check_win(self, line: list[Union[int, str]], win_value: int) -> Optional[Union[int, str]]:
+        """Checks if a line has all the same markers to see if the game has been won."""
+        counter = Counter(line)
+        value, count = counter.most_common(1)[0]
+        if count == win_value and value != 0:
+            return value  # Return the value of the winning marker (e.g., 'x' or 'o')
+        return None  # No winner
+
+
+    def _check_rows(self, win_value: int) -> Optional[Union[int, str]]:
+        """Checks for winner in rows. Uses returned Square object to update the winner attributes. False if no Square
+        object is assigned. """
+        for r, row in enumerate(self.get_rows()):
+            if winner := self._check_win(row, win_value):
+                # self._update_winner_info(winner, "row", r)
+                return winner, "row", r, 0
+
+    def _check_columns(self, win_value: int) -> Optional[Union[int, str]]:
+        """Checks for winner in columns. Uses returned Square object to update winner attributes. False if no Square
+        object is assigned. """
+        for c, column in enumerate(self.get_columns()):
+            if winner := self._check_win(column, win_value):
+                # self._update_winner_info(winner, "col", c)
+                return winner, "column", 0, c
+
+    def _check_diagonals(self, win_value: int) -> Optional[Union[int, str]]:
+        """Checks for winner in diagonals. Uses returned Square object to update winner attributes. False if no
+        Square object is assigned. """
+        for l, line in enumerate(self.get_diagonals(win_value, "right")):
+            if winner := self._check_win(line, win_value):
+            # self._update_winner_info(winner, "right_diag")
+                r, c = int_converter(l, win_value)
+                return winner, "right_diagonal", r, c
+        for l, line in enumerate(self.get_diagonals(win_value, "left")):
+            if winner := self._check_win(line, win_value):
+            # self._update_winner_info(winner, "left_diag")
+                r, c = int_converter(l, win_value)
+                c = self.columns - c - 2
+                return winner, "left_diagonal", r, c
+            
+    def check_for_winner(self, win_value: int) -> Optional[Union[int, str]]:
+        """Checks if the game has been won in a row, column, or diagonal."""
+        for check_func in (self._check_rows, self._check_columns, self._check_diagonals):
+            if winner_found := check_func(win_value):
+                return winner_found
+        return None  
+    
+    def __str__(self) -> str:
+        """Returns a more readable board representation."""
+        return "\n".join([" ".join(str(cell) for cell in row) for row in self.board])
 
     def __repr__(self) -> str:
-        """Returns a string of information on current attributes of the board for information purposes only."""
-        return f'Board:\n{str(self.board)}'
+        return f"Board({self.rows}x{self.columns})\n{self.__str__()}"
+
 
 test=Board(6,7)
-for row in test.get_rows():
-    print(row)
-diagonals = test.get_right_diagonals(4)
+test.update_square(2,3,"x")
+test.update_square(3,3,"x")
+test.update_square(4,3,"x")
+test.update_square(5,3,"x")
+test.update_square(4, 6, "o")
+print(test)
+a, b, c ,d = test.check_for_winner(4)
+winner_info(a, b, c, d)
+test.reset_board()
+
+# Right diagonal winner configuration (from top-left to bottom-right)
+test.update_square(0, 0, "x")
+test.update_square(1, 1, "x")
+test.update_square(2, 2, "x")
+test.update_square(3, 3, "x")
+test.update_square(4, 6, "o")
+print(test)
+a, b, c ,d = test.check_for_winner(4)
+winner_info(a, b, c, d)
+
+# Left diagonal winner (random position from top-right to bottom-left)
+test.reset_board()
+test.update_square(2, 4, "x")  # First "x" in the diagonal
+test.update_square(3, 3, "x")  # Second "x" in the diagonal
+test.update_square(4, 2, "x")  # Third "x" in the diagonal
+test.update_square(5, 1, "x")  # Fourth "x" in the diagonal
+test.update_square(4, 6, "o")  # Just a random "o" in another spot
+print(test)
+a, b, c ,d = test.check_for_winner(4)
+winner_info(a, b, c, d)
+
+# Right diagonal winner (random position from top-left to bottom-right)
+test.reset_board()
+test.update_square(1, 1, "x")  # First "x" in the diagonal
+test.update_square(2, 2, "x")  # Second "x" in the diagonal
+test.update_square(3, 3, "x")  # Third "x" in the diagonal
+test.update_square(4, 4, "x")  # Fourth "x" in the diagonal
+test.update_square(5, 0, "o")  # Just a random "o" in another spot
+print(test)
+a, b, c ,d = test.check_for_winner(4)
+winner_info(a, b, c, d)
+# print(test._check_rows(4))
+# print(test._check_columns(4))
+
+# for row in test.get_rows():
+#     print(row)
+# diagonals = test.get_diagonals(4, "right")
+# for i in diagonals:
+#     print(i)
+# diagonals = test.get_diagonals(4, "left")
+# for i in diagonals:
+#     print(i)
+# test.reset_board()
+# for row in test.get_rows():
+#     print(row)
+
+# diagonals = test.get_right_diagonals(4)
+# for i in diagonals:
+#     print(i)
+
 
 # def board(rows: int, columns: int):
 #     board = []
@@ -100,3 +220,15 @@ diagonals = test.get_right_diagonals(4)
 #     def reset_board(self) -> None:
 #         """Sets each square in the board to a blank."""
 #         self.board = [[0] * 3 for _ in range(3)]
+
+# board = [
+#     [ 1,  2,  3,  4],
+#     [ 5,  6,  7,  8],
+#     [ 9, 10, 11, 12],
+#     [13, 14, 15, 16]
+# ]
+        # for i in range(self.rows):
+        #     if i == 1 or i == 5:
+        #         board.append([8//n for n in range(1, self.columns + 1)])
+        #     else:
+        #         board.append([0 for _ in range(self.columns)])
